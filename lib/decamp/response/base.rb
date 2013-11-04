b0d9a2b8acafea23509ecaaccb89ddf8668a0a86
robhurring <robhurring@gmail.com>
2013-11-04 17:43:36 -0500

    parse out special info keys

--- lib/decamp/response/base.rb
+++ lib/decamp/response/base.rb
@@ -15,17 +15,17 @@ module Decamp
       end
 
       def title
-        @title ||= response.xpath('//table[1]/tr[1]').text.strip
+        @title ||= clean_text(response.xpath('//table[1]/tr[1]'))
       end
 
       def stops
-        @stops ||= response.xpath('//table[1]/tr[2]/td').map{ |node| node.text.strip }
+        @stops ||= response.xpath('//table[1]/tr[2]/td').map{ |node| clean_text(node) }
       end
 
       def routes
         @routes ||= begin
           response.xpath('//table[1]/tr[position()>2]').map do |route| 
-            route.xpath('./td').map{ |node| node.text.strip }
+            route.xpath('./td').map{ |node| clean_text(node) }
           end
         end
       end
@@ -34,6 +34,28 @@ module Decamp
         @timetable ||= begin
           routes.map{ |r| Hash[stops.zip(r)] }
         end
+      end
+
+      def special
+        @special ||= begin
+          # nokogiri handles &nbsp's weird, so we need to gsub these out
+          nbsp = Nokogiri::HTML("&nbsp;").text
+
+          data = response.xpath('//table[2]/tr').map do |row|
+            key = clean_text(row.xpath('./td[1]')).gsub(nbsp, '')
+            value = clean_text(row.xpath('./td[2]')).gsub(nbsp, '')
+
+            [key, value]
+          end
+          Hash[data]
+        end
+      end
+
+    protected
+
+      def clean_text(node_or_string)
+        node_or_string = node_or_string.text if node_or_string.respond_to?(:text)
+        node_or_string.strip
       end
     end
   end
