c50039298c77d06634668ee056f2cfb486ef8771
robhurring <robhurring@gmail.com>
2013-11-08 11:16:34 -0500

    segregate shared examples

--- spec/support/shared_a_scraped_schedule.rb
+++ spec/support/shared_a_scraped_schedule.rb
@@ -1,11 +1,14 @@
 shared_examples 'a scraped schedule' do |route, options = {}|
-  options[:has_routes] = true if options[:has_routes].nil?
   options[:day] ||= described_class::WEEKDAY
   options[:time] ||= described_class::ALL_DAY
 
+  around(:each) do |example|
+    VCR.use_cassette(['schedule', route, options[:day], options[:time]].compact.join('-'), &example)
+  end
+
   let(:scraper){ described_class.new(route, day: options[:day], time: options[:time]) }
 
-  context 'with a fetched schedule', vcr: {cassette_name: ['schedule', route, options[:day], options[:time]].compact.join('-') } do
+  context 'with a fetched schedule' do
     describe 'fetching' do
       it 'finds the schedule' do
         response = scraper.fetch
@@ -19,7 +22,7 @@ shared_examples 'a scraped schedule' do |route, options = {}|
       end
     end
 
-    describe 'the response' do
+    describe 'the basic response' do
       let!(:response){ scraper.fetch }
 
       it 'should have a title' do
@@ -29,24 +32,39 @@ shared_examples 'a scraped schedule' do |route, options = {}|
       it 'should have stops' do
         response.stops.should_not be_empty
       end
-
-      if options[:has_routes]
-        it 'should have routes' do
-          response.routes.should_not be_empty
-        end
-
-        it 'should have a timetable' do
-          response.timetable.should_not be_empty
-        end            
-      else
-        it 'should not have routes' do
-          response.routes.should be_empty
-        end
-
-        it 'should not have a timetable' do
-          response.timetable.should be_empty
-        end    
-      end
     end
   end
 end
+
+shared_examples 'a schedule with routes' do |route, options = {}|
+  it_should_behave_like "a scraped schedule", route, options do
+    describe 'the timetable' do
+      let!(:response){ scraper.fetch }
+
+      it 'should have routes' do
+        response.routes.should_not be_empty
+      end
+
+      it 'should have a timetable' do
+        response.timetable.should_not be_empty
+      end            
+    end
+  end
+end
+
+shared_examples 'a schedule with no routes' do |route, options = {}|
+  it_should_behave_like "a scraped schedule", route, options do
+    describe 'the timetable' do
+      let!(:response){ scraper.fetch }
+
+      it 'should have routes' do
+        response.routes.should be_empty
+      end
+
+      it 'should have a timetable' do
+        response.timetable.should be_empty
+      end            
+    end
+  end
+end
+
